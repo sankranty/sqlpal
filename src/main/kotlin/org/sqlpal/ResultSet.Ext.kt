e1c266d99a7a5eaacba646ce14a6a38204c7da14
sankranti <sankranty@gmail.com>
2026-04-07 18:10:07 +0300

    Added JavaDoc comments for public ResultSet extension methods

--- src/main/kotlin/org/sqlpal/ResultSet.Ext.kt
+++ src/main/kotlin/org/sqlpal/ResultSet.Ext.kt
@@ -6,37 +6,88 @@ import java.time.*
 
 // Public ResultSet extension methods
 
+/** Returns value provided by [getValue], or null if actual values is null.
+ * Is used for nullable columns as JDBC methods does not return null for not-nullable Java types. */
 inline fun <T> ResultSet.valueOrNull(getValue: () -> T): T? {
     val value: T = getValue()
     return if (wasNull()) null else value
 }
 
 // Methods that return value for NULL (should be used only for NOT NULL columns)
-infix fun ResultSet.intVal(str: String) = getInt(str)
-infix fun ResultSet.longVal(str: String) = getLong(str)
-infix fun ResultSet.byteVal(str: String) = getByte(str)
-infix fun ResultSet.shortVal(str: String) = getShort(str)
-infix fun ResultSet.doubleVal(str: String) = getDouble(str)
-infix fun ResultSet.floatVal(str: String) = getFloat(str)
-infix fun ResultSet.boolVal(str: String) = getBoolean(str)
+
+/** Reads Int value from NOT NULL column with specified name. */
+infix fun ResultSet.intVal(colName: String) = getInt(colName)
+
+/** Reads Long value from NOT NULL column with specified name. */
+infix fun ResultSet.longVal(colName: String) = getLong(colName)
+
+/** Reads Byte value from NOT NULL column with specified name. */
+infix fun ResultSet.byteVal(colName: String) = getByte(colName)
+
+/** Reads Short value from NOT NULL column with specified name. */
+infix fun ResultSet.shortVal(colName: String) = getShort(colName)
+
+/** Reads Double value from NOT NULL column with specified name. */
+infix fun ResultSet.doubleVal(colName: String) = getDouble(colName)
+
+/** Reads Float value from NOT NULL column with specified name. */
+infix fun ResultSet.floatVal(colName: String) = getFloat(colName)
+
+/** Reads Boolean value from NOT NULL column with specified name. */
+infix fun ResultSet.boolVal(colName: String) = getBoolean(colName)
 
 // Methods that return null for NULL
-infix fun ResultSet.int(str: String) = valueOrNull { this intVal str }
-infix fun ResultSet.long(str: String) = valueOrNull { this longVal str }
-infix fun ResultSet.byte(str: String) = valueOrNull { this byteVal str }
-infix fun ResultSet.short(str: String) = valueOrNull { this shortVal str }
-infix fun ResultSet.double(str: String) = valueOrNull { this doubleVal str }
-infix fun ResultSet.float(str: String) = valueOrNull { this floatVal str }
-infix fun ResultSet.bool(str: String) = valueOrNull { this boolVal str }
 
-infix fun ResultSet.str(str: String): String? = getString(str)
-infix fun ResultSet.dec(str: String): BigDecimal? = getBigDecimal(str)
-infix fun ResultSet.date(str: String): LocalDate? = getObject(str, LocalDate::class.java)
-infix fun ResultSet.time(str: String): LocalTime? = getObject(str, LocalTime::class.java)
-infix fun ResultSet.dt(str: String): LocalDateTime? = getObject(str, LocalDateTime::class.java)
-infix fun ResultSet.dto(str: String): OffsetDateTime? = getObject(str, OffsetDateTime::class.java)
-infix fun ResultSet.dtz(str: String): ZonedDateTime? = getObject(str, OffsetDateTime::class.java)?.toZonedDateTime()
+/** Reads Int value from nullable column with specified name. */
+infix fun ResultSet.int(colName: String) = valueOrNull { this intVal colName }
+
+/** Reads Long value from nullable column with specified name. */
+infix fun ResultSet.long(colName: String) = valueOrNull { this longVal colName }
+
+/** Reads Byte value from nullable column with specified name. */
+infix fun ResultSet.byte(colName: String) = valueOrNull { this byteVal colName }
+
+/** Reads Short value from nullable column with specified name. */
+infix fun ResultSet.short(colName: String) = valueOrNull { this shortVal colName }
+
+/** Reads Double value from nullable column with specified name. */
+infix fun ResultSet.double(colName: String) = valueOrNull { this doubleVal colName }
+
+/** Reads Float value from nullable column with specified name. */
+infix fun ResultSet.float(colName: String) = valueOrNull { this floatVal colName }
+
+/** Reads Boolean value from nullable column with specified name. */
+infix fun ResultSet.bool(colName: String) = valueOrNull { this boolVal colName }
+
+
+/** Reads String value from column with specified name. */
+infix fun ResultSet.str(colName: String): String? = getString(colName)
+
+/** Reads BigDecimal value from column with specified name. */
+infix fun ResultSet.dec(colName: String): BigDecimal? = getBigDecimal(colName)
+
+/** Reads LocalDate value from column with specified name. */
+infix fun ResultSet.date(colName: String): LocalDate? = getObject(colName, LocalDate::class.java)
+
+/** Reads LocalTime value from column with specified name. */
+infix fun ResultSet.time(colName: String): LocalTime? = getObject(colName, LocalTime::class.java)
+
+/** Reads LocalTime value from column with specified name. */
+infix fun ResultSet.otime(colName: String): OffsetTime? = getObject(colName, OffsetTime::class.java)
+
+/** Reads LocalDateTime value from column with specified name. */
+infix fun ResultSet.dt(colName: String): LocalDateTime? = getObject(colName, LocalDateTime::class.java)
+
+/** Reads OffsetDateTime value from column with specified name. */
+infix fun ResultSet.odt(colName: String): OffsetDateTime? = getObject(colName, OffsetDateTime::class.java)
+
+/** Reads ZonedDateTime value from column with specified name. */
+infix fun ResultSet.zdt(colName: String): ZonedDateTime? = getObject(colName, OffsetDateTime::class.java)?.toZonedDateTime()
 
 // Methods for enum type
-inline infix fun <reified T: Enum<T>> ResultSet.enum(str: String): T? = getString(str)?.let { enumValueOf<T>(it) }
-inline infix fun <reified T: Enum<T>> ResultSet.enumVal(str: String): T = enumValueOf(getString(str))
+
+/** Reads enum value from nullable column with specified name. */
+inline infix fun <reified T: Enum<T>> ResultSet.enum(colName: String): T? = getString(colName)?.let { enumValueOf<T>(it) }
+
+/** Reads enum value from NOT NUL column with specified name. */
+inline infix fun <reified T: Enum<T>> ResultSet.enumVal(colName: String): T = enumValueOf(getString(colName))
